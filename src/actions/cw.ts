70d6dc0a4a4a59ec639236be72c84c3965e28819
aryehgreenspan <agreenspan@digacore.com>
2025-08-04 17:20:48 -0400

    Update cw.ts

--- src/actions/cw.ts
+++ src/actions/cw.ts
@@ -1,40 +1,26 @@
 "use server";
 
-import { cookies } from "next/headers";
-import { env } from "~/env";
+import { ConnectWise } from "connectwise-rest";
 
-export const cwGet = async <TReturnType,>(
-	stub: string,
-	conditions?: string,
-) => {
-	const base = `https://${env.CW_COMPANY_URL}/${env.CW_CODE_BASE}/apis/3.0`;
-	return fetch(
-		conditions ? `${base}${stub}?conditions=${conditions}` : `${base}${stub}`,
-		{
-			headers: {
-				clientId: env.CW_CLIENT_ID,
-				Cookie: cookies().toString(),
-			},
-		},
-	).then((res) => res.json() as Promise<TReturnType | undefined>);
-};
+// This checks if the required environment variables are set.
+if (
+  !process.env.CW_COMPANY_URL ||
+  !process.env.CW_CLIENT_ID ||
+  !process.env.CW_PRIVATE_KEY
+) {
+  throw new Error("ConnectWise credentials are not set in environment variables.");
+}
 
-export const cwPost = async <TReturnType,>(
-	stub: string,
-	body: string,
-) => {
-	const base = `https://${env.CW_COMPANY_URL}/${env.CW_CODE_BASE}/apis/3.0`;
-	return fetch(
-		`${base}${stub}`,
-		{
-			headers: {
-				clientId: env.CW_CLIENT_ID,
-				Cookie: cookies().toString(),
-				'Content-Type': 'application/json'
-			},
-			body,
-			method: 'POST'
-		},
+// This initializes the ConnectWise client with your credentials.
+// It is the 'cw' object that other files need to import.
+export const cw = new ConnectWise({
+  companyUrl: process.env.CW_COMPANY_URL,
+  clientId: process.env.CW_CLIENT_ID,
+  privateKey: process.env.CW_PRIVATE_KEY,
+});
 
-	).then((res) => res.json() as Promise<TReturnType | undefined>);
-};
+// This is a helper function for making generic API GET requests.
+export async function cwGet<T>(path: string): Promise<T> {
+  const response = await cw.api(path, "GET");
+  return response.data as T;
+}
